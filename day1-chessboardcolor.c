93c55105bb6a91823349fd685ad4063d052e4238
Akshara Gupta <gakshara21@gmail.com>
2026-01-28 15:57:43 +0530

    day1-chessboardcolor.c
    Above is the standard representation of a chessboard.
    
    This could be imagined as a 2D cartesian plane, with the x axis being represented by the alphabets and y axis by the numbers.
    
    Given coordinates in the form of string, print if that cell is white or black.
    
    Input Format
    First line contains a string s.
    
    Output Format
    White or Black.

--- day1-chessboardcolor.c
+++ day1-chessboardcolor.c
@@ -1,17 +1,4 @@
-ques-https://d8it4huxumps7.cloudfront.net/uploads/courseDetails/628db68ec030d_picture_1.png
-Above is the standard representation of a chessboard.
 
-This could be imagined as a 2D cartesian plane, with the x axis being represented by the alphabets and y axis by the numbers.
-
-Given coordinates in the form of string, print if that cell is white or black.
-
-Input Format
-First line contains a string s.
-
-Output Format
-White or Black.
-
-Solution-
 #include <stdio.h>
 #include <string.h>
 
